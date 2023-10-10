a6fc4dc70218e73dcef3882026670206b22c5cde
Karl Lewis Doctolero <karllewistdoctolero@gmail.com>
2023-10-10 15:54:36 +0800

    update_welcomeblade_file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,6 +33,10 @@
                 </div>
             @endif
 
+
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+
+
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
 
 
