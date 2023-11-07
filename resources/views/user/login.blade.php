1696a6fbe39257a36b9c172017bf58cb1fad454f
Karl Lewis Doctolero <karllewistdoctolero@gmail.com>
2023-11-08 01:47:12 +0800

    msg

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -2,7 +2,7 @@
 
     <header class= "max-w-lg mx-auto">
         <a href="">
-            <h1 class="text-4xl font-bold text-white text-center">Student Login</h1>
+            <h1 class="text-4xl font-bold text-white text-center">Admin Login</h1>
         </a>
     </header>
 
